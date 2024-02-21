483b9929d2c1ca42858ee435faab012a1cfa2ca9
rizalkororo <safrizal@kororo.co>
2024-02-21 15:59:24 +0700

    Add CRUD operation

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  root "articles#index"
+  resources :articles
 end
